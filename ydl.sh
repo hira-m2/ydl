5d3d57b64dd229cd9b4f58452a91cf34eff0b79b
mehdi-m2 <mehdi.mahmoudi.s@gmail.com>
2015-07-23 23:48:48 +0430

    Edit get proxy

--- ydl.sh
+++ ydl.sh
@@ -16,14 +16,21 @@ fi
 #end check
 
 
+check=true
+while [ $check = true ]
+do
+	echo "Do you use proxy for accesse to youtube?(y/n)"
+	read proxy_use
 
-echo "Do you use proxy for accesse to youtube?(y/n)"
-read proxy_use
-
-if [ 'y' = '$proxy_use'];then
-	echo "Enter your proxy:(user:pass@hoet:port)"
-	read proxy
-fi
+	if [ "y" = "$proxy_use" ]; then
+		echo "Enter your proxy:(user:pass@hoet:port)"
+		read proxy
+		echo $proxy #for debug
+		check=false
+	elif [ "n" = "$proxy_use" ]; then
+		check=false
+	fi
+done
 #export http_proxy="127.0.0.1:8123"
 #export https_proxy="127.0.0.1:8123"
 
