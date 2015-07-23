bdf3919e6dc3d5b46b51bfeb63da0e54d071545a
mehdi-m2 <mehdi.mahmoudi.s@gmail.com>
2015-07-24 02:23:51 +0430

    Add downlaod script

--- ydl.sh
+++ ydl.sh
@@ -4,6 +4,7 @@
 
 
 
+
 #check for install youtube-dl
 
 problem=$(dpkg -s youtube-dl|grep installed)
@@ -16,31 +17,55 @@ fi
 #end check
 
 
-check=true
-while [ $check = true ]
+
+# Get Proxy	
+bool=true
+while [ $bool = true ]
 do
 	echo "Do you use proxy for accesse to youtube?(y/n)"
 	read proxy_use
 
 	if [ "y" = "$proxy_use" ]; then
-		echo "Enter your proxy:(user:pass@hoet:port)"
-		read proxy
-		echo $proxy #for debug
-		check=false
+		echo "Enter your http proxy:(user:pass@host:port)"
+		read hproxy
+		echo "Enter your https proxy:(user:pass@host:port)"
+		read sproxy
+		bool=false
 	elif [ "n" = "$proxy_use" ]; then
-		check=false
+		bool=false
 	fi
 done
-#export http_proxy="127.0.0.1:8123"
-#export https_proxy="127.0.0.1:8123"
 
-#cd ~/Downloads/youtube/
-#wget -c $*
-#ret_code=1
-#while [ $ret_code -ne 0 ]
-#do
-#	youtube-dl $*
-#	ret_code=$?
-#	echo $ret_code
-#	sleep 23
-#	done
+#Create File in /usr/bin for easy run script
+
+sudo touch /usr/bin/ydl
+sudo chmod 777 /usr/bin/ydl
+
+
+
+#Insert code to /usr/bin/ydl
+
+echo "#!/bin/sh" > /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "#Set proxy" >> /usr/bin/ydl
+echo "export http_proxy=\"$hproxy\"" >> /usr/bin/ydl
+echo "export https_proxy=\"$sproxy\"" >> /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "#Go to Download directory" >> /usr/bin/ydl
+echo "cd ~/Downloads/youtube/" >> /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "" >> /usr/bin/ydl
+echo "#Retry Download until Download completed" >> /usr/bin/ydl
+echo "ret_code=1" >> /usr/bin/ydl
+echo "while [ \$ret_code -ne 0 ]" >> /usr/bin/ydl
+echo "do" >> /usr/bin/ydl
+echo "	youtube-dl \$*" >> /usr/bin/ydl
+echo "#Get return code of youtube-dl" >> /usr/bin/ydl
+echo "	ret_code=\$?" >> /usr/bin/ydl
+echo "	sleep 100" >> /usr/bin/ydl
+echo "done" >> /usr/bin/ydl
+
+sudo chmod 755 /usr/bin/ydl
+	
