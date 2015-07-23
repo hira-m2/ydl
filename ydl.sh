8e94af1e14c77df9c02a6ae3640be7acf81a2631
mehdi-m2 <mehdi.mahmoudi.s@gmail.com>
2015-07-23 23:17:19 +0430

    checking for youtube-dl & proxy

--- ydl.sh
+++ ydl.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
+# 
+# /usr/bin  path to add script
 
 
-export http_proxy="127.0.0.1:8123"
-export https_proxy="127.0.0.1:8123"
 
-cd ~/Downloads/youtube/
+#check for install youtube-dl
+
+problem=$(dpkg -s youtube-dl|grep installed)
+echo "Checking for youtube-dl: $problem"
+if [ "" = "$problem" ]; then
+	echo "Youtube-dl do not install.\nSetting up libxul"
+	sudo apt-get --force-yes --yes install youtube-dl 
+fi
+
+#end check
+
+
+
+echo "Do you use proxy for accesse to youtube?(y/n)"
+read proxy_use
+
+if [ 'y' = '$proxy_use'];then
+	echo "Enter your proxy:(user:pass@hoet:port)"
+	read proxy
+fi
+#export http_proxy="127.0.0.1:8123"
+#export https_proxy="127.0.0.1:8123"
+
+#cd ~/Downloads/youtube/
 #wget -c $*
-ret_code=1
-while [ $ret_code -ne 0 ]
-do
-	youtube-dl $*
-	ret_code=$?
-	echo $ret_code
-	sleep 23
-done
+#ret_code=1
+#while [ $ret_code -ne 0 ]
+#do
+#	youtube-dl $*
+#	ret_code=$?
+#	echo $ret_code
+#	sleep 23
+#	done
